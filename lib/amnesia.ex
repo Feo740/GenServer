ebb8d977002a47f971fd4ff972604a638575cbe6
Feo740 <feo2006@yandex.ru>
2017-09-16 18:56:17 +0300

    first step with mnesia

---
+++ lib/amnesia.ex
@@ -0,0 +1,26 @@
+defmodule Amn do
+
+
+  def start_database do
+    :mnesia.create_schema([node()])#создаем БД,в рабочем кат.
+                                  #появится папка Mnesia.nonode@nohost
+    :mnesia.start()               #в этом каталоге созд файл schema.DAT
+    :mnesia.create_table(P_players,[{:attributes,[:nikname, :name, :age, :games, :wins]}])
+  end
+
+  def wr_database do
+  :mnesia.transaction(fn ->
+    :mnesia.write({P_players, "hat", "Bill Smith", "45", 100, 40})
+    :mnesia.write({P_players, "bell", "Ann Pirson", "20", 10, 3})
+    :mnesia.write({P_players, "Rat", "Den Konnie", "45", 75, 37})
+  end)
+  end
+
+  def rd_database(nik) do
+    {:atomic, [record]} = :mnesia.transaction(fn ->
+  :mnesia.read({P_players, nik}) end)
+  end
+  def stop_database do
+    :mnesia.stop()
+  end
+end
