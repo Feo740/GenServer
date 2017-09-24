86e2c1101dbc9cc4cd1a5dfabf6646893ad7ef82
Feo740 <feo2006@yandex.ru>
2017-09-24 18:11:25 +0300

    Добавили автотесты для нашей БД

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Serv.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      description: description(), #
+      package: package(),
+      deps: deps(),
+      name: "Database_serv",
+      source_url: "https://github.com/Feo740/GenServer"
     ]
   end
 
@@ -21,10 +25,30 @@ defmodule Serv.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, github: "elixir-lang/ex_doc"},
-      {:markdown, github: "devinus/markdown"}
+    {:ex_doc, ">= 0.0.0", only: :dev},
+          #{:ex_doc, github: "elixir-lang/ex_doc"},
+  #  {:markdown, github: "devinus/markdown"}
+      #{:serv, "0.1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
+
+  defp description() do
+    "Учебный проект создания базы данных с использованием максимального количества возможностей Erlang/Elixir"
+  end
+
+  defp package() do
+    [
+      #Эту опцию ставим только если не хотим пользоваться именем OTP приложения
+      #name: "Database_serv",
+      # Сюда файлы, которые по умолчанию будут в пакете
+      files: ["lib", "mix.exs", "test", "doc", "deps", "config", "README.md"],
+      maintainers: ["Feodor Terekhov"],
+      licenses: ["OpenSource"],
+      links: %{"GitHub" => "https://github.com/Feo740/GenServer"}
+    ]
+  end
+
+
 end
--- test/serv_test.exs
+++ test/serv_test.exs
@@ -1,8 +1,25 @@
 defmodule ServTest do
-  use ExUnit.Case
+  use ExUnit.Case # async: true #async:true - позволяем Эликсир выполнять наборы тестов параллельно
   doctest Serv
 
-  test "greets the world" do
-    assert Serv.hello() == :world
+  setup_all do  # общий сценарий к выполнению, перед выполнением тестов
+    IO.puts "Beginning all tests"
+    ServSUP.start_l
+    :timer.sleep(3_000)
+    Serv.start_database
+    :timer.sleep(2_000)
+
+    on_exit fn ->
+        IO.puts "Exit from all tests"
+      end
+      {:ok,[]}
+  end
+
+  test "Read from your perfect database" do
+    assert Serv.zapros("hat") == {:ok, {:atomic, [{:p_players, "hat", "Bill Smith", "45", 100, 40}]}}
+  end
+
+  test "Wrong question to your perfect database" do
+    assert Serv.zapros("_rat") == {:ok, %MatchError{term: {:atomic, []}}}
   end
 end
